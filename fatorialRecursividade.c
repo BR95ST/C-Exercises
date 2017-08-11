c0719b9abc8d9bea0cfb4b6bf6764b9ebd24ae60
Bruno <br95st@protonmail.com>
2017-08-11 10:58:35 -0300

    Update fatorialRecursividade.c

--- fatorialRecursividade.c
+++ fatorialRecursividade.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
-int fatorial(int n)
+int factorial(int n)
 {
     if(n == 1)
         return n;
     else
-        return (n * fatorial(n - 1));
+        return (n * factorial(n - 1));
 }
 int main(void)
 {
     int num;
-    puts("Digite um numero inteiro: ");
+    printf("Enter an integer: ");
     scanf("%d",&num);
-    printf("%d\n", fatorial(num));
+    printf("Factorial: %d\n", factorial(num));
 }
