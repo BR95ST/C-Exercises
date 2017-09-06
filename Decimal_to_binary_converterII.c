54e8d7987fe61c898d06ffe6c5e178d7f7cd6989
Bruno <br95st@protonmail.com>
2017-09-06 14:57:26 -0300

    Update Decimal_to_binary_converterII.c

--- Decimal_to_binary_converterII.c
+++ Decimal_to_binary_converterII.c
@@ -3,23 +3,23 @@
 int checarBits(int x){
 	int cont = 0;
 	
-	if(x == 0 || x == 1){
-		return x;
-	}
-	else{
-		cont++;
-		return cont + checarBits(x /2);   	
-	}	
+	if(x == 0 || x == 1)
+		return x;	
+	else
+		return ++cont + checarBits(x /2); 	
 }
 void converterValor(int x, int y){
 	int bin[y], i;
-			
-	for(i=0;i<y;i++){
-		bin[i] = x % 2;
-		x /= 2;
-	}
-	for(i=y-1;i>=0;i--){
-		printf("%d", bin[i]);
+	if(x == 0)
+		printf("%d", x);	
+	else{
+		for(i=0;i<y;i++){
+			bin[i] = x % 2;
+			x /= 2;
+		}
+		for(i=y-1;i>=0;i--){
+			printf("%d", bin[i]);
+		}
 	}
 }
 int main(){
