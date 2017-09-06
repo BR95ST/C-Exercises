a80e4aa058c8275b3c087a07c3b7b405acee5355
Bruno <br95st@protonmail.com>
2017-09-06 14:24:09 -0300

    Add files via upload

---
+++ Decimal_to_binary_converterII.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int checarBits(int x){
+	int cont = 0;
+	
+	if(x == 0 || x == 1){
+		return x;
+	}
+	else{
+		cont++;
+		return cont + checarBits(x /2);   	
+	}	
+}
+void converterValor(int x, int y){
+	int bin[y], i;
+			
+	for(i=0;i<y;i++){
+		bin[i] = x % 2;
+		x /= 2;
+	}
+	for(i=y-1;i>=0;i--){
+		printf("%d", bin[i]);
+	}
+}
+int main(){
+	int num;
+	
+	printf("Digite um numero Decimal: ");
+	scanf("%d", &num);
+	
+	converterValor(num, checarBits(num));
+	
+	return 0;
+}
